c60e189734fbcbc1818c794071eac1e7920c5137
dina12118 <85133414+dina12118@users.noreply.github.com>
2023-08-22 15:42:37 +0300

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,7 @@
+#Write a Python program to test whether a passed letter is a vowel or not.
+
+letter = input("write a letter: ")
+if letter not in ['o','a','u','i','a']:
+    print("Not a vowel letter")
+else:
+    print("VOWEL")
